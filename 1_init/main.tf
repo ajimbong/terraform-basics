5faae9ba22f6b5029cd2b3bc56101b40364e5591
ajimbong <ajimbong713@gmail.com>
2023-07-13 15:30:46 +0100

    initial commit

---
+++ 1_init/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# resource "aws_instance" "ec2" {
+#   ami = "ami-06ca3ca175f37dd66"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "Hello Terra"
+#   }
+# }
+
+
+# ---- Using terraform to reference other resources
+# By creating a vpc and a subnet
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+	tags = {
+  		Name = "demo-vpc"
+	}
+}
+
+resource "aws_subnet" "demo-subnet-1" {
+    vpc_id = aws_vpc.demo-vpc.id
+    cidr_block = "10.0.1.0/24"
+  
+  tags = {
+      Name = "demo-subnet-1"
+  }
+}
+
+# Print out useful information (like ip addresses) when you you run [terraform apply]
+
+output "subnet-cidr" {
+  value = aws_subnet.demo-subnet-1.cidr_block
+}
